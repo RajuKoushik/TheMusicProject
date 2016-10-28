22c63566f6492e992199e17808de64eb5599bc06
rajukoushik <g.rajukoushik@gmail.com>
2016-10-29 00:53:33 +0530

    Initial commit

---
+++ TheMusicApp/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ThemusicappConfig(AppConfig):
+    name = 'TheMusicApp'
---
+++ TheMusicProject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TheMusicProject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TheMusicProject.settings")
+
+application = get_wsgi_application()
