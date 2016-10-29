19dbef7ed0c92807f40a7bc6a5897769bf245349
rajukoushik <g.rajukoushik@gmail.com>
2016-10-29 17:00:16 +0530

    Updated Temlates.

--- TheMusicApp/views.py
+++ TheMusicApp/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Album
 
 def index(request):
 
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id)
-        html = html + '<a href="' + str(url)+'">' + str(album.album_title)+ '</a> <br>'
-    return HttpResponse(html)
+    template = loader.get_template('TheMusicApp/index.html')
+
+    #context has all the variables that are passed into teh context
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context,request))
 
 def details(request, album_id):
     return HttpResponse("<h1>You are at the album "+ str(album_id)+ " .</h1>")
